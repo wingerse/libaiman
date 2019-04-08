f7f0f9988a08f614354ace4bd368583eb73c6fdc
winger_sendon <aiman012@live.com>
2019-04-09 01:16:21 +0800

    fix name style

--- src/containers/hashes.c
+++ src/containers/hashes.c
@@ -1,4 +1,4 @@
 #include <libaiman/containers/hashes.h>
 
-extern inline bool HashmapStringEq(const string *a, const string *b);
-extern inline u32 HashmapStringHash(const string *k);
+extern inline bool Hashmap_string_eq(const string *a, const string *b);
+extern inline u32 string_hash(const string *k);
