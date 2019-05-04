2bec0f153a74aeecf41100806e9762f24b600b06
winger_sendon <aiman012@live.com>
2019-05-05 02:11:01 +0800

    array size

--- include/libaiman/prelude.h
+++ include/libaiman/prelude.h
@@ -8,6 +8,7 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 // lets you know if a pointer is owning (need to free/destroy).
 // only for documentation purposes
